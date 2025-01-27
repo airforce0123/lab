66a07936ae44c5defd5f272d880203407d676ea0
airforce <revanthchintu03@gmail.com>
2025-01-27 19:49:58 +0530

    Update 2-1.py

--- 2-1.py
+++ 2-1.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers
-model = tf.keras.Sequential([layers.Dense(64, activation='relu', input_shape=(784,)),  # Input layer layers.Dense(10, activation='softmax')])
+model = tf.keras.Sequential([layers.Dense(64, activation='relu', input_shape=(784,)),] ) 
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 import numpy as np
 train_data = np.random.random((1000, 784))  # 1000 samples, 784 features each
